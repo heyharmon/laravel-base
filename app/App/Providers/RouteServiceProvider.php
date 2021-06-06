12d3612064749f961e8de397ca765102153177cb
Ryan Harmon <heyharmon@gmail.com>
2021-06-05 18:45:40 -0600

    Update RouteServiceProvider.php

--- app/App/Providers/RouteServiceProvider.php
+++ app/App/Providers/RouteServiceProvider.php
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    protected $namespace = 'App\\Http';
+    protected $namespace = 'DDD\\Http';
 
     /**
      * Define your route model bindings, pattern filters, etc.
