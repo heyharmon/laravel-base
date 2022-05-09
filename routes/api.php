734e5d939171ac529f34d1c14d5e331dd2443efa
Ryan Harmon <heyharmon@gmail.com>
2022-05-09 01:06:06 -0600

    Cleanup routes

--- routes/api.php
+++ routes/api.php
@@ -7,11 +7,13 @@ use DDD\Http\Sites\SiteController;
 use DDD\Http\Pages\PageController;
 
 // Sites
-Route::get('/sites',                [SiteController::class, 'index']);
-Route::post('/sites',               [SiteController::class, 'store']);
-Route::get('/sites/{site:host}',    [SiteController::class, 'show']);
-Route::delete('/sites/{site:host}', [SiteController::class, 'destroy']);
+Route::get('/sites',           [SiteController::class, 'index']);
+Route::post('/sites',          [SiteController::class, 'store']);
+Route::get('/sites/{site}',    [SiteController::class, 'show']);
+Route::delete('/sites/{site}', [SiteController::class, 'destroy']);
 
 // Pages
-Route::get('/sites/{site:host}/pages',  [PageController::class, 'index']);
-Route::post('/sites/{site:host}/pages', [PageController::class, 'store']);
+Route::prefix('/sites/{site}')->group(function () {
+    Route::get('/pages',  [PageController::class, 'index']);
+    Route::post('/pages', [PageController::class, 'store']);
+});
