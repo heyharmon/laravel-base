dbe393018bbabf94db6ee7855fc1250927f31919
Ryan Harmon <heyharmon@gmail.com>
2022-06-18 02:29:24 -0600

    Cleanup

--- app/App/Traits/IsNestable.php
+++ app/App/Traits/IsNestable.php
@@ -2,7 +2,31 @@
 
 namespace DDD\App\Traits;
 
+// Vendors
+use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
+
 trait IsNestable
 {
+    use HasRecursiveRelationships;
 
+    /**
+     * Get the nested children associated with this model.
+     *
+     * @return hasMany
+     */
+    public function children()
+    {
+        return $this->hasMany(self::class, 'parent_id');
+    }
+
+    /**
+     * Get only parent (top level) tags.
+     *
+     * @param Builder $query
+     * @return Builder
+     */
+    public function scopeParents($query)
+    {
+        return $query->whereNull('parent_id');
+    }
 }
--- app/Domain/Tags/Tag.php
+++ app/Domain/Tags/Tag.php
@@ -5,40 +5,17 @@ namespace DDD\Domain\Tags;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-// Vendors
-use Staudenmeir\LaravelAdjacencyList\Eloquent\HasRecursiveRelationships;
-
 // Traits
 use DDD\App\Traits\HasSlug;
+use DDD\App\Traits\IsNestable;
 
 class Tag extends Model
 {
     use HasFactory,
         HasSlug,
-        HasRecursiveRelationships;
+        IsNestable;
 
     protected $guarded = [
         'id',
     ];
-
-    /**
-     * Get the children tags associated with this tag.
-     *
-     * @return hasMany
-     */
-    public function children()
-    {
-        return $this->hasMany(Tag::class, 'parent_id');
-    }
-
-    /**
-     * Get only parent (top level) tags.
-     *
-     * @param Builder $query
-     * @return Builder
-     */
-    public function scopeParents($query)
-    {
-        return $query->whereNull('parent_id');
-    }
 }
