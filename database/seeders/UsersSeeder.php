a16dae9f64c62e34c69d7a06d0a2daa2eed23ad5
Ryan Harmon <heyharmon@gmail.com>
2022-06-23 16:05:32 -0600

    Update user seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -18,23 +18,44 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
+        $admins = [
+            [
+                'name' => 'Test Admin',
+                'email' => 'test@email.com',
+                'role' => 'admin',
+                'organization_id' => 1,
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => 'Ryan Harmon',
+                'email' => 'ryan@bloomcu.com',
+                'role' => 'admin',
+                'organization_id' => 1,
+                'email_verified_at' => now(),
+                'password' => Hash::make('6MyUHw[7(J4pdBBe'),
+            ],
+            [
+                'name' => 'Derik Krauss',
+                'email' => 'derik@bloomcu.com',
+                'role' => 'admin',
+                'organization_id' => 1,
+                'email_verified_at' => now(),
+                'password' => Hash::make('eynwKg8D$7zh[~HS'),
+            ]
+        ];
+
+        foreach ($admins as $admin) {
+            User::create($admin);
+        }
+
         $faker = \Faker\Factory::create();
 
-        User::create([
-            'name' => 'Test Admin',
-            'email' => 'test@email.com',
-            'role' => 'admin',
-            'organization_id' => 1,
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
-            'remember_token' => Str::random(10),
-        ]);
-
-        foreach (range(1, 19) as $i) {
+        foreach (range(1, 20) as $i) {
             User::create([
                 'name' => $faker->name,
                 'email' => $faker->unique()->safeEmail,
-                'role' => 'admin',
+                'role' => 'editor',
                 'organization_id' => rand(1, 3),
                 'email_verified_at' => now(),
                 'password' => Hash::make('password'),
