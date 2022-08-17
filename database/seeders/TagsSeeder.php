1e766ba3abafa3b4bceb4fd9e2c581daae2f3e3e
Ryan Harmon <heyharmon@gmail.com>
2022-08-17 16:55:26 -0600

    Update tags seeder

--- database/seeders/TagsSeeder.php
+++ database/seeders/TagsSeeder.php
@@ -23,109 +23,94 @@ class TagsSeeder extends Seeder
                 'id' => 1,
                 'title' => 'Content Tags',
             ],
-            [
-                'id' => 2,
-                'title' => 'Accounts',
-                'parent_id' => 1,
-            ],
-            [
-                'id' => 3,
-                'title' => 'Checking',
-                'parent_id' => 2,
-            ],
-            [
-                'id' => 4,
-                'title' => 'Savings',
-                'parent_id' => 2,
-            ],
-            [
-                'id' => 5,
-                'title' => 'Share',
-                'parent_id' => 4,
-            ],
-            [
-                'id' => 6,
-                'title' => 'Money Market',
-                'parent_id' => 4,
-            ],
-            [
-                'id' => 7,
-                'title' => 'Certificates',
-                'parent_id' => 4,
-            ],
-            [
-                'id' => 8,
-                'title' => 'Loans',
-                'parent_id' => 1,
-            ],
-            [
-                'id' => 9,
-                'title' => 'Cards',
-                'parent_id' => 1,
-            ],
+                [
+                    'id' => 2,
+                    'title' => 'Accounts',
+                    'parent_id' => 1,
+                ],
+                [
+                    'id' => 3,
+                    'title' => 'Checking',
+                    'parent_id' => 2,
+                ],
+                [
+                    'id' => 4,
+                    'title' => 'Savings',
+                    'parent_id' => 2,
+                ],
+                [
+                    'id' => 5,
+                    'title' => 'Share',
+                    'parent_id' => 4,
+                ],
+                [
+                    'id' => 6,
+                    'title' => 'Money Market',
+                    'parent_id' => 4,
+                ],
+                [
+                    'id' => 7,
+                    'title' => 'Certificates',
+                    'parent_id' => 4,
+                ],
+                [
+                    'id' => 8,
+                    'title' => 'Loans',
+                    'parent_id' => 1,
+                ],
+                [
+                    'id' => 9,
+                    'title' => 'Cards',
+                    'parent_id' => 1,
+                ],
             [
                 'id' => 10,
-                'title' => 'Content Types',
+                'title' => 'Content Type Tags',
             ],
-            [
-                'id' => 11,
-                'title' => 'Product',
-                'parent_id' => 10,
-            ],
-            [
-                'id' => 12,
-                'title' => 'Service',
-                'parent_id' => 10,
-            ],
-            [
-                'id' => 13,
-                'title' => 'Info',
-                'parent_id' => 10,
-            ],
+                [
+                    'id' => 11,
+                    'title' => 'Product',
+                    'parent_id' => 10,
+                ],
+                [
+                    'id' => 12,
+                    'title' => 'Service',
+                    'parent_id' => 10,
+                ],
+                [
+                    'id' => 13,
+                    'title' => 'Info',
+                    'parent_id' => 10,
+                ],
             [
                 'id' => 14,
                 'title' => 'Media Tags',
             ],
-            [
-                'id' => 15,
-                'title' => 'Brand',
-                'parent_id' => 14,
-            ],
-            [
-                'id' => 16,
-                'title' => 'Logo',
-                'parent_id' => 14,
-            ],
-            [
-                'id' => 17,
-                'title' => 'Brand',
-                'parent_id' => 14,
-            ],
-            [
-                'id' => 18,
-                'title' => 'Fonts',
-                'parent_id' => 14,
-            ],
-            [
-                'id' => 19,
-                'title' => 'Desktop Fonts',
-                'parent_id' => 18,
-            ],
-            [
-                'id' => 20,
-                'title' => 'Web Fonts',
-                'parent_id' => 18,
-            ],
-            [
-                'id' => 21,
-                'title' => 'Photos',
-                'parent_id' => 14,
-            ],
-            [
-                'id' => 22,
-                'title' => 'Other Media',
-                'parent_id' => 14,
-            ],
+                [
+                    'id' => 15,
+                    'title' => 'Brand',
+                    'parent_id' => 14,
+                ],
+                [
+                    'id' => 16,
+                    'title' => 'Logo',
+                    'parent_id' => 14,
+                ],
+                [
+                    'id' => 17,
+                    'title' => 'Fonts',
+                    'parent_id' => 14,
+                ],
+                [
+                    'id' => 18,
+                    'title' => 'Photos',
+                    'parent_id' => 14,
+                ],
+                [
+                    'id' => 19,
+                    'title' => 'Other Media',
+                    'parent_id' => 14,
+                ],
         ];
 
         foreach ($tags as $tag) {
