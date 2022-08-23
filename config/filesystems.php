e55a722e5ccf8679d7ea92039e8c0e7e039cbf90
Ryan Harmon <heyharmon@gmail.com>
2022-08-23 09:33:15 -0600

    Updates to filesystem config and .env.example to enable AWS S3

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,6 +51,7 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
+            'visibility' => 'public',
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
