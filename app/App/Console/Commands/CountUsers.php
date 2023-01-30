e63cdfe67f8b3bfa73a87ecceb86ec6148858d00
Ryan Harmon <heyharmon@gmail.com>
2023-01-30 14:28:19 -0700

    Clean up commands

---
+++ app/App/Console/Commands/CountUsers.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DDD\App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use DDD\Domain\Users\User;
+
+class CountUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'count:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Count total number of users';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $count = User::count();
+
+        $this->info('Total users: ' . $count);
+    }
+}
