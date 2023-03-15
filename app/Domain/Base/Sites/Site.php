5c14da6668a9c1733c525390920076cea33e03b3
Ryan Harmon <ryan@bloomcu.com>
2023-03-15 02:15:56 -0600

    Minor cleanup

--- app/Domain/Base/Sites/Site.php
+++ app/Domain/Base/Sites/Site.php
@@ -3,7 +3,6 @@
 namespace DDD\Domain\Base\Sites;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 // Casts
