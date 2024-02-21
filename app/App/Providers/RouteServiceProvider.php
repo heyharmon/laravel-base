d4dd9b394394a32f3867b70b24b999f9c7e6c89b
Shift <shift@laravelshift.com>
2024-02-21 18:10:49 +0000

    Adopt class based routes

--- app/App/Providers/RouteServiceProvider.php
+++ app/App/Providers/RouteServiceProvider.php
@@ -40,16 +40,13 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/base/api.php'));
 
             Route::prefix('api')
                 ->middleware('api')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
