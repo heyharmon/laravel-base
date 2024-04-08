583e3b33dad4ff3726ccf80e8846bd3b79c549ed
Ryan Harmon <ryan@bloomcu.com>
2024-04-08 15:56:20 -0600

    Add factory trait to File model

--- app/Domain/Base/Files/File.php
+++ app/Domain/Base/Files/File.php
@@ -3,18 +3,20 @@
 namespace DDD\Domain\Base\Files;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use DDD\App\Traits\BelongsToUser;
 use DDD\App\Traits\BelongsToOrganization;
 
 class File extends Model
 {
     use BelongsToOrganization,
-        BelongsToUser;
+        BelongsToUser,
+        HasFactory;
 
     protected $guarded = ['id'];
 
     public function getStorageUrl()
     {
-        return config('cdn.cdn_url').'/'.$this->path;
+        return config('cdn.cdn_url') . '/' . $this->path;
     }
 }
